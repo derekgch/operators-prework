3f7a2de02066428faaac39b415b7bf2e84fa2eb0
derek chen <derek.g.chen@gmail.com>
2018-03-20 20:32:27 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,10 @@
 def unsafe?(speed)
-	if speed > 60
-		return true
-	elsif speed < 40
-		return true
-	else
+	if speed <60 && speed > 40
 		return false
+	else
+		return return
 	end
 end
-
 
 
 def not_safe?(speed)
